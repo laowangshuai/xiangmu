990d24d793d8c20250db5678ea09ba58cebeb26f
unknown <629175914@qq.com>
2021-06-01 16:51:43 +0800

    张恩赐更改冲突后版本

--- src/components/jiajian.vue
+++ src/components/jiajian.vue
@@ -8,19 +8,6 @@
 		name:'jiajian',
 	 
 	}
-
-	<div>
-		<h2>{{$store.state.num}}</h2>
-	</div>
-<<<<<<< HEAD
-</template>
-
-<script>
-=======
-	
-     
->>>>>>> 300df727a84b286b527df19f16c21dcc906b2de4
-	
 </script>
 
 <style>
